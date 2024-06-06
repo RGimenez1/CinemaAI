4abc61af753e6ca84b26de0346eb5e8fcabdcaa0
Renato Gimenez <renatogimenez96@gmail.com>
2024-06-06 16:35:17 -0300

    including basic get route for movies

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,11 +1,13 @@
-
-from fastapi import APIRouter
-from app.services.movie_service import get_all_movies
-from typing import List
+from fastapi import APIRouter, Query
+from app.services.movie_service import get_movies_by_title
+from typing import List, Optional
 from app.models.movie import Movie
 
 router = APIRouter()
 
+
 @router.get("/movies", response_model=List[Movie])
-async def read_movies():
-    return await get_all_movies()
+async def read_movies(
+    title: Optional[str] = Query(None, description="Title of the movie to search")
+):
+    return await get_movies_by_title(title)
--- app/services/movie_service.py
+++ app/services/movie_service.py
@@ -1,9 +1,15 @@
-
 from app.repository.database import database
 from app.models.movie import Movie
-from typing import List
+from typing import List, Optional
 
-async def get_all_movies() -> List[Movie]:
+
+async def get_movies_by_title(title: Optional[str]) -> List[Movie]:
     movies_collection = database.get_collection("movies")
-    movies = await movies_collection.find().to_list(100)
+    query = {}
+    if title:
+        query["title"] = {
+            "$regex": title,
+            "$options": "i",
+        }  # Case-insensitive regex search
+    movies = await movies_collection.find(query).to_list(100)
     return [Movie(**movie) for movie in movies]
